fc4786aa5231dc3fe77596651bbaa92783c10e16
GianmarcoArz <gianmarcoarzanese@gmail.com>
2024-10-22 15:40:40 +0200

    fine

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,5 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrl: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  user: { username: string; password: string } = {
+    username: '',
+    password: '',
+  };
+}
